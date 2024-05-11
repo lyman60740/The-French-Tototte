02cb98b933eaa645aea41a8823eebd490586ad93
Lyman <lym_60740@hotmail.fr>
2024-05-11 18:55:21 +0200

    integration plug section

--- src/js/hoverEffects.js
+++ src/js/hoverEffects.js
@@ -134,4 +134,120 @@ document.addEventListener("DOMContentLoaded", function () {
     personnalisableAppearImg.reverse();
     personnalisableAppearBg.reverse();
   });
+
+  const made = document.querySelector(".bento__box__made");
+
+  let madeAppearImg = gsap.to(".bento__box__made img", {
+    duration: 0.4,
+    scale: 1.1,
+    rotate: -5,
+    ease: "power1.inOut",
+    paused: true,
+  });
+
+  let madeAppearBg = gsap.to(".bento__box__made__bg", {
+    duration: 0.3,
+    autoAlpha: 1,
+    ease: "power1.inOut",
+    paused: true,
+  });
+
+  made.addEventListener("mouseenter", function () {
+    madeAppearImg.play();
+    madeAppearBg.play();
+  });
+
+  made.addEventListener("mouseleave", function () {
+    madeAppearImg.reverse();
+    madeAppearBg.reverse();
+  });
+
+  const innovante = document.querySelector(".bento__box__innovante");
+
+  let innovanteAppearImg = gsap.to(".bento__box__innovante img", {
+    duration: 0.4,
+    yPercent: -10,
+    rotate: 2,
+    ease: "power1.inOut",
+    paused: true,
+  });
+
+  let innovanteAppearBg = gsap.to(".bento__box__innovante__bg", {
+    duration: 0.3,
+    yPercent: -20,
+    ease: "power1.inOut",
+    paused: true,
+  });
+
+  innovante.addEventListener("mouseenter", function () {
+    innovanteAppearImg.play();
+    innovanteAppearBg.play();
+  });
+
+  innovante.addEventListener("mouseleave", function () {
+    innovanteAppearImg.reverse();
+    innovanteAppearBg.reverse();
+  });
+
+  const savoureuse = document.querySelector(".bento__box__savoureuse");
+
+  let savoureuseAppearImg = gsap.to(".bento__box__savoureuse img", {
+    duration: 0.8,
+    xPercent: -12,
+    ease: "power3.inOut",
+    paused: true,
+  });
+
+  savoureuse.addEventListener("mouseenter", function () {
+    savoureuseAppearImg.play();
+  });
+
+  savoureuse.addEventListener("mouseleave", function () {
+    savoureuseAppearImg.reverse();
+  });
+
+  const hebergement = document.querySelector(".bento__box__hebergement");
+
+  let hebergementAppearImg = gsap.to(".bento__box__hebergement__img", {
+    duration: 0.4,
+    height: "80px",
+    ease: "power1.inOut",
+    paused: true,
+  });
+
+  let hebergementAppearBg = gsap.to(".bento__box__hebergement__bg", {
+    duration: 0.3,
+    autoAlpha: 1,
+    ease: "power1.inOut",
+    paused: true,
+  });
+
+  hebergement.addEventListener("mouseenter", function () {
+    hebergementAppearImg.play();
+    hebergementAppearBg.play();
+  });
+
+  hebergement.addEventListener("mouseleave", function () {
+    hebergementAppearImg.reverse();
+    hebergementAppearBg.reverse();
+  });
+
+  const underline = document.querySelector(".plug__discover span");
+
+  underline.addEventListener("mouseenter", function () {
+    gsap.to(".plug__discover__underline", {
+      duration: 0.9,
+      scaleX: 1,
+      ease: "elastic.out(1.5,0.6)",
+    });
+  });
+
+  underline.addEventListener("mouseleave", function () {
+    gsap.to(".plug__discover__underline", {
+      duration: 0.3,
+      scaleX: 0,
+      ease: "linear",
+      overwrite: "auto",
+    });
+  });
 });
